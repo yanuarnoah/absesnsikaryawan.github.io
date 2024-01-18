df74ee5214b8a0ddc9cd00365d0c7d675600ee85
yanuarnoah <157047140+yanuarnoah@users.noreply.github.com>
2024-01-19 05:12:41 +0700

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,4 @@
+<?php 
+mysql_connect("sql207.epizy.com","epiz_27861789","MNEx8YoOfS4cKqd");
+mysql_select_db("epiz_27861789_dbpenjualan");
+?>
---
+++ penjualan.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2020 at 09:07 PM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `malasngoding_kios`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(30) NOT NULL,
+  `pass` varchar(70) NOT NULL,
+  `foto` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `uname`, `pass`, `foto`) VALUES
+(8, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'text.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE IF NOT EXISTS `barang` (
+  `id` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `jenis` text NOT NULL,
+  `suplier` text NOT NULL,
+  `modal` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `sisa` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `jenis`, `suplier`, `modal`, `harga`, `jumlah`, `sisa`) VALUES
+(14, 'roti unibis', 'makanan', 'pt.makamur jaya', 5000, 6500, 350, 20),
+(17, 'tim tam', 'makanan ringan', 'pt surga', 2000, 6000, 791, 10),
+(19, 'tic tac', 'makanan ringan', 'pt sido multp', 2000, 4000, 2, 24),
+(20, 'aqua sedang', 'minuman ringan', 'pt jamaika', 1000, 3000, 990, 1000),
+(21, 'makkkanan', 'makanan ringan', 'pt surya', 2000, 4000, 894, 900),
+(22, 'makkkanan', 'makanan ringan', 'pt surya', 2000, 4000, 894, 900),
+(23, 'magnum', 'rokok', 'pt surga', 12000, 13000, 997, 1000),
+(24, 'santri mie', 'makanan ringan', 'pt sido mulyo', 2000, 4000, 784, 800),
+(25, 'rambut palsu', 'accesories', 'pt ptan', 3000, 5000, 496, 500),
+(26, 'rambut palsu', 'accesories', 'pt ptan', 3000, 5000, 496, 500),
+(27, 'sea foog', 'makanan ringan', 'pt surga', 4000, 60000, 598, 600),
+(28, 'mild', 'rokok', 'pt sampoerna', 15000, 17000, 192, 200),
+(29, 'dji sam soe', 'rokok', 'pt ptan', 14000, 15000, 145, 150),
+(30, 'nu mild', 'rokok', 'pt ptan', 14000, 15000, 144, 150),
+(31, 'nu mild', 'rokok', 'pt ptan', 14000, 15000, 144, 150),
+(32, 'roti', 'makanan', 'hdhnhnhh', 2000, 5000, 4, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_laku`
+--
+
+CREATE TABLE IF NOT EXISTS `barang_laku` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama` text NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `total_harga` int(20) NOT NULL,
+  `laba` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang_laku`
+--
+
+INSERT INTO `barang_laku` (`id`, `tanggal`, `nama`, `jumlah`, `harga`, `total_harga`, `laba`) VALUES
+(46, '2015-02-01', 'roti unibis', 2, 6000, 12000, 2000),
+(47, '2015-02-02', 'makkkanan', 7, 12000, 84000, 70000),
+(48, '2015-02-02', 'dji sam soe', 2, 15000, 30000, 2000),
+(49, '2015-02-03', 'makkkanan', 1, 12000, 12000, 10000),
+(50, '2015-02-01', 'tim tam', 2, 4000, 8000, 4000),
+(51, '2015-02-02', 'mild', 2, 17000, 34000, 4000),
+(52, '2015-02-03', 'magnum', 1, 18000, 18000, 6000),
+(53, '2015-02-06', 'dji sam soe', 2, 19000, 38000, 10000),
+(54, '2015-02-15', 'nu mild', 2, 19100, 38200, 10200),
+(55, '2015-02-27', 'roti unibis', 2, 8000, 16000, 6000),
+(56, '2015-02-19', 'roti unibis', 1, 7000, 7000, 2000),
+(57, '2015-01-14', 'roti unibis', 1, 7000, 7000, 2000),
+(58, '2015-02-01', 'pulpen', 1, 3000, 3000, 2000),
+(59, '2015-02-02', 'roti', 2, 3000, 6000, 2000),
+(63, '2016-01-22', 'tic tac', 8, 4000, 32000, 16000),
+(64, '0000-00-00', 'roti unibis', 0, 0, 0, 0),
+(65, '2019-07-02', 'tim tam', 1, 120000, 120000, 118000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengeluaran`
+--
+
+CREATE TABLE IF NOT EXISTS `pengeluaran` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `keperluan` text NOT NULL,
+  `nama` text NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id`, `tanggal`, `keperluan`, `nama`, `jumlah`) VALUES
+(1, '2015-02-06', 'de', 'diki', 1234);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `barang_laku`
+--
+ALTER TABLE `barang_laku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+--
+-- AUTO_INCREMENT for table `barang_laku`
+--
+ALTER TABLE `barang_laku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=66;
+--
+-- AUTO_INCREMENT for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
